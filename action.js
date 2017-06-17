f78cc8890916e4bfb3b7e00ce9e334cd3fc3f2a5
Lo Yu-Cheng <david855033@gmail.com>
2017-06-17 10:09:57 +0800

    ui adjust

--- action.js
+++ action.js
@@ -48,7 +48,7 @@ $(window).resize(function() {
             app.isMenuOnTop=false;
             app.isMenuShowed=true;     
       }
-      $(".content").width(bodyWidth-165);
+      $(".content").width(bodyWidth-150);
   }
 });
 $(function() {
@@ -61,7 +61,7 @@ $(function() {
   {
     app.isMenuOnTop=false;
     app.isMenuShowed=true;
-    $(".content").width(bodyWidth-165);
+    $(".content").width(bodyWidth-150);
   }
 });
 $(function(){
