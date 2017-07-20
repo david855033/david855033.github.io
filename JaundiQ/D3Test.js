523cc570d1e61c4ee5128a41404cf7045a71c89e
Lo Yu-Cheng <david855033@gmail.com>
2017-07-20 12:34:39 +0800

    domperidone

--- JaundiQ/D3Test.js
+++ JaundiQ/D3Test.js
@@ -1,3 +1,6 @@
+'use strict';
+var testData=[1,2,3,4,5,6,7,1,2,3,4,5,1,3,3,4];
+var his=d3.histogram().domain([2,5]).thresholds(d3.ticks(2,5,4));
+console.log(testData);
+console.log(JSON.stringify (his(testData)));
 
-d3.select(".axis")
-    .call(d3.axisBottom(d3.scaleLinear().domain([0,10]).range([0,300])));
