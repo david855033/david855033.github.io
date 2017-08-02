a34525142c966cd796ffef85c562ccb2532aa627
Lo Yu-Cheng <david855033@gmail.com>
2017-08-02 18:46:54 +0800

    .

--- JaundiQ/D3Test.js
+++ JaundiQ/D3Test.js
@@ -1,5 +1,24 @@
 'use strict';
-var testData=[1,2,3,4,5,6,7,1,2,3,4,5,1,3,3,4];
-var his=d3.histogram().domain([2,5]).thresholds(d3.ticks(2,5,4));
-console.log(testData);
-console.log(JSON.stringify(his(testData)));
+var svgContainer = d3.select("body")
+  .append("svg")
+    .attr("width", 500)
+    .attr("height", 200)
+    .style("padding", "10px")
+    .style("border", "1px solid");
+
+var xScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([0, 500]);
+
+    	
+var xAxis = d3.axisBottom(xScale)
+    .ticks(5)
+    .tickPadding(2);
+
+var height=200, padding=10;
+
+svgContainer.append("g")
+    .attr("transform", function() {
+        return "translate(" + 0 + "," + (height - padding) + ")"
+     })
+     .call(xAxis);
