336f3295857306e62f1d0bee2a69241c72437473
Lo Yu-Cheng <david855033@gmail.com>
2017-08-07 22:04:09 +0800

    NTISS

--- NTISS/vm.js
+++ NTISS/vm.js
@@ -1,9 +1,19 @@
-
 var vm = new Vue(
 {
     el:'#app',
     data:{
-        leftCol:[
+        name:'恩提斯之女',
+        patientNo:'40236547',
+        birthday:'2017-08-07',
+        currentDate:'2017-08-07',
+        recordList:[
+            {"date":'2017-08-07',"score":25},
+            {"date":'2017-08-08',"score":20},
+            {"date":'2017-08-09',"score":15},
+        ],
+        colNumber:3,
+        showDetail:true,
+        tables:[
             {
                 tableName:"Respiratory",
                 items:[
@@ -16,7 +26,6 @@ var vm = new Vue(
                         score:3,group:'A',selected:false},
                     {name:'Mechanical Ventilation with Muscle Relaxation', info:'',
                         score:4,group:'A',selected:false},
-                    {name:'',info:'',score:'',group:'',selected:false},
                     {name:'High-frequency Ventilation', info:'',
                         score:4,group:'A',selected:false},
                     {name:'',info:'',score:'',group:'',selected:false},
@@ -32,33 +41,6 @@ var vm = new Vue(
                         score:1,group:'B',selected:false},
                     {name:'',info:'',score:'',group:'',selected:false},
                     {name:'Extracorporeal Membrane Oxygenation', info:'',
-                        score:4,group:'',selected:false},
-                    {name:'',info:'',score:'',group:'',selected:false}
-                ]
-            },
-            {
-                tableName:"Cardiovascular",
-                items:[
-                    {name:'',info:'',score:'',group:'',selected:false},
-                    {name:'Indomethacin Administration', info:'',
-                        score:1,group:'',selected:false,spacing:true},
-                    {name:'',info:'',score:'',group:'',selected:false},
-                    {name:'Volume Expansion ≤ 15ml/kg', info:'',
-                        score:1,group:'C',selected:false},
-                    {name:'Volume Expansion > 15ml/kg', info:'',
-                        score:3,group:'C',selected:false},
-                    {name:'',info:'',score:'',group:'',selected:false},
-                    {name:'Vasopressor Administration(1 agent)', info:'',
-                        score:2,group:'D',selected:false},
-                    {name:'Vasopressor Administration(>1 agent)', info:'',
-                        score:3,group:'D',selected:false},
-                    {name:'',info:'',score:'',group:'',selected:false},
-                    {name:'Pacemaker on Standby', info:'',
-                        score:3,group:'E',selected:false},
-                    {name:'Pacemaker Used', info:'',
-                        score:4,group:'E',selected:false},
-                    {name:'',info:'',score:'',group:'',selected:false},
-                    {name:'Cardiopulmonary Resustation', info:'',
                         score:4,group:'',selected:false},
                     {name:'',info:'',score:'',group:'',selected:false}
                 ]
@@ -93,6 +75,33 @@ var vm = new Vue(
                 ]
             },
             {
+                tableName:"Cardiovascular",
+                items:[
+                    {name:'',info:'',score:'',group:'',selected:false},
+                    {name:'Indomethacin Administration', info:'',
+                        score:1,group:'',selected:false,spacing:true},
+                    {name:'',info:'',score:'',group:'',selected:false},
+                    {name:'Volume Expansion ≤ 15ml/kg', info:'',
+                        score:1,group:'C',selected:false},
+                    {name:'Volume Expansion > 15ml/kg', info:'',
+                        score:3,group:'C',selected:false},
+                    {name:'',info:'',score:'',group:'',selected:false},
+                    {name:'Vasopressor Administration(1 agent)', info:'',
+                        score:2,group:'D',selected:false},
+                    {name:'Vasopressor Administration(>1 agent)', info:'',
+                        score:3,group:'D',selected:false},
+                    {name:'',info:'',score:'',group:'',selected:false},
+                    {name:'Pacemaker on Standby', info:'',
+                        score:3,group:'E',selected:false},
+                    {name:'Pacemaker Used', info:'',
+                        score:4,group:'E',selected:false},
+                    {name:'',info:'',score:'',group:'',selected:false},
+                    {name:'Cardiopulmonary Resustation', info:'',
+                        score:4,group:'',selected:false},
+                    {name:'',info:'',score:'',group:'',selected:false}
+                ]
+            },
+            {
                 tableName:"Monitoring",
                 items:[
                     {name:'Frequent Vital Signs', info:'',
@@ -116,23 +125,15 @@ var vm = new Vue(
                         score:2,group:'H',selected:false},
                     {name:'',info:'',score:'',group:'',selected:false}
                 ]
-            }
-        ],
-        rightCol:[
+            },
             {
-                tableName:"Metabolic Nutrition",
+                tableName:"Vascular Access",
                 items:[
-                    {name:'Gavage Feeding', info:'',
-                        score:1,group:'',selected:false},
-                    {name:'Intravenous Amino Acid Solution', info:'',
-                        score:1,group:'',selected:false},
-                    {name:'Intravenous Fat Emulsion', info:'',
-                        score:1,group:'',selected:false},
-                    {name:'Phototherapy', info:'',
-                        score:1,group:'',selected:false},
-                    {name:'Potassium Infusion', info:'',
-                        score:3,group:'',selected:false},
-                    {name:'Insulin Administration', info:'',
+                    {name:'Peripheral Intravanous Line', info:'',
+                        score:1,group:'',selected:true},
+                    {name:'Central Venous Line', info:'',
+                        score:2,group:'',selected:false},
+                    {name:'Arterial Line', info:'',
                         score:2,group:'',selected:false},
                     {name:'',info:'',score:'',group:'',selected:false}
                 ]
@@ -189,13 +190,19 @@ var vm = new Vue(
                 ]
             },
             {
-                tableName:"Vascular Access",
+                tableName:"Metabolic Nutrition",
                 items:[
-                    {name:'Peripheral Intravanous Line', info:'',
-                        score:1,group:'',selected:true},
-                    {name:'Central Venous Line', info:'',
-                        score:2,group:'',selected:false},
-                    {name:'Arterial Line', info:'',
+                    {name:'Gavage Feeding', info:'',
+                        score:1,group:'',selected:false},
+                    {name:'Intravenous Amino Acid Solution', info:'',
+                        score:1,group:'',selected:false},
+                    {name:'Intravenous Fat Emulsion', info:'',
+                        score:1,group:'',selected:false},
+                    {name:'Phototherapy', info:'',
+                        score:1,group:'',selected:false},
+                    {name:'Potassium Infusion', info:'',
+                        score:3,group:'',selected:false},
+                    {name:'Insulin Administration', info:'',
                         score:2,group:'',selected:false},
                     {name:'',info:'',score:'',group:'',selected:false}
                 ]
@@ -206,9 +213,44 @@ var vm = new Vue(
     {
         totalScore:function(){
             var sum=0;
-            sum+=d3.sum(this.leftCol.map(x=>d3.sum(x.items,y=>y.selected?y.score:0)));
-            sum+=d3.sum(this.rightCol.map(x=>d3.sum(x.items,y=>y.selected?y.score:0)));
+            sum=d3.sum(this.tables.map(x=>d3.sum(x.items,y=>y.selected?y.score:0)));
             return sum;
+        },
+        col1Table:function(){
+            if(this.colNumber==1)
+            {
+                return [0,7];
+            }else if (this.colNumber==2)
+            {
+                return [0,3];
+            }else if (this.colNumber==3)
+            {
+                return [0,1];
+            }
+        },
+        col2Table:function(){
+            if(this.colNumber==1)
+            {
+                return [8,8];
+            }else if (this.colNumber==2)
+            {
+                return [4,7];
+            }else if (this.colNumber==3)
+            {
+                return [2,4];
+            }
+        },
+        col3Table:function(){
+            if(this.colNumber==1)
+            {
+                return [8,8];
+            }else if (this.colNumber==2)
+            {
+                return [8,8];
+            }else if (this.colNumber==3)
+            {
+                return [5,7];
+            }
         }
     },
     methods:
@@ -225,3 +267,34 @@ var vm = new Vue(
     }
 });
 
+var renderCols=function(){
+    var windowWidth = $(window).width();
+    var app=$('#app');
+    var col1=$('#col1');
+    var col2=$('#col2');
+    var col3=$('#col3');
+    if(windowWidth>=1450){
+        app.width(1450);
+        col1.css('width',"33%");
+        col2.css('width',"33%");
+        col3.css('width',"33%");
+        vm.colNumber=3;
+    }else if(windowWidth>=1050)
+    {
+        app.width(1050);
+        col1.css('width',"50%");
+        col2.css('width',"50%");
+        col3.css('width',"0%");
+        vm.colNumber=2;
+    }else if(windowWidth>=850)
+    {
+        app.width(850);
+        col1.css('width',"100%");
+        col2.css('width',"0%");
+        col3.css('width',"0%");
+        vm.colNumber=1;
+    }
+}
+
+$(window).resize(renderCols);
+$(renderCols);
